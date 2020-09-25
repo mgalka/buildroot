mgalka/buildroot

commit 5d06ce715b0342f7a15d2e4ca3ac0d638456c64b

bootrr: upgrade with support for ACPI and new devices
Upgrade bootrr to kernelci-20200925 which adds support for determining
the platform name on ACPI devices, and test scripts for the "hana" and
"grunt" Chromebooks.

Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

package/bootrr/bootrr.hash
@@ -1,2 +1,2 @@
 # Locally calculated
-sha256 f125029bff0cc659b9073b993c710a14994d9cd46de9b29ea5d205b6b1997173  bootrr-e0a316de59153ecd4373b14063b0280505ce6e2e.tar.gz
+sha256 ab4e26ea846c0d99dd3008c491e0f56fc0fa91d0871fb54d1a9f97b212d480a2  bootrr-fdd52e16572ec16811a1303f4d781d9ba4195f07.tar.gz

package/bootrr/bootrr.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 BOOTRR_SITE = https://github.com/kernelci/bootrr.git
-BOOTRR_VERSION = e0a316de59153ecd4373b14063b0280505ce6e2e
+BOOTRR_VERSION = fdd52e16572ec16811a1303f4d781d9ba4195f07 # kernelci-20200925
 BOOTRR_SITE_METHOD = git
 BOOTRR_LICENSE = LGPL-2.1+
 

commit 2761b6781b6176e2d1302e36274f6a6f8b48af17

bootrr: fix updated revision
The previous commit was using a revision from the main branch whereas
the PRs to add the scripts were merged on staging.kernelci.org, so
they were not included.  This has now been fixed in the bootrr
repository, so update the revision again.

Fixes: 5d06ce715b03 ("bootrr: upgrade with support for ACPI and new devices")
Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

package/bootrr/bootrr.hash
@@ -1,2 +1,2 @@
 # Locally calculated
-sha256 ab4e26ea846c0d99dd3008c491e0f56fc0fa91d0871fb54d1a9f97b212d480a2  bootrr-fdd52e16572ec16811a1303f4d781d9ba4195f07.tar.gz
+sha256 2b711a75986b0ee981c3fb9433fa5b961c1f0a85fedcec9e744becd431a4bc4c  bootrr-809288b2a1f9530e44ed7ae306fb76908427cfbc.tar.gz

package/bootrr/bootrr.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 BOOTRR_SITE = https://github.com/kernelci/bootrr.git
-BOOTRR_VERSION = fdd52e16572ec16811a1303f4d781d9ba4195f07 # kernelci-20200925
+BOOTRR_VERSION = 809288b2a1f9530e44ed7ae306fb76908427cfbc # kernelci-20200925.1
 BOOTRR_SITE_METHOD = git
 BOOTRR_LICENSE = LGPL-2.1+
 