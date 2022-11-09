244d245c9a25868ef10b51f6187c4ec30a0c4d70
Michal Galka <michal.galka@collabora.com>
2022-11-09 11:41:09 +0100

    HACK: change bootrr repo

--- package/bootrr/bootrr.mk
+++ package/bootrr/bootrr.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-BOOTRR_SITE = https://github.com/kernelci/bootrr.git
-BOOTRR_VERSION = f00862c8406f81ef62449b5c0267e010e3be3d3f
+BOOTRR_SITE = https://github.com/mgalka/bootrr.git
+BOOTRR_VERSION = f5b8545334729dd63ee687daaf9d6cbcd8db5f6a
 BOOTRR_SITE_METHOD = git
 BOOTRR_LICENSE = LGPL-2.1+
 
